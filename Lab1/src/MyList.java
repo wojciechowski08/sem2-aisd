d1a206b06b6f9f24b6fef38bb9138189036af4e4
Daniel Wojciechowski <wojciechowski.daniel.1998@gmail.com>
2018-03-07 09:22:17 +0100

    adding first lab

---
+++ Lab1/src/MyList.java
@@ -0,0 +1,147 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MyList<T> implements Iterable<T> {
+	
+private Object[] data;
+private int default_size;
+
+	public MyList(T anArray[]) {
+		data = anArray;
+	}
+	
+	public int size() {
+		int s = 0;
+		for (int i = 0; i < data.length; i++) {
+			if (data[i] != null) {
+				s++;
+			}
+		}
+		return s;
+	}
+	
+	public boolean isEmpty() {
+		for (int i = 0; i < data.length; i++) {
+			if (data[i] != null) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public boolean contains(T o) {
+		for (int i =0; i < data.length; i++) {
+			if ( data[i].equals(o) ) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public int indexOf(T o) {
+		int i = 0;
+		for (; i < data.length; i++) {
+			if ( data[i].equals(o) ) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public Object get(int i) {
+		if (i < data.length && i >= 0) {
+			return data[i];
+		} else {
+			return null;
+		}
+	}
+	
+	public boolean set(int index, T element) {
+		if (index < data.length && index >= 0) {
+			data[index] = element;
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+	
+	public boolean add(T elem) {
+		Object[] temp = new Object[data.length + 1];
+		int i = 0;
+		for (; i < data.length; i++) {
+			temp[i] = data[i];
+		}
+		temp[i+1] = elem;
+		data = temp;
+		return true;
+	}
+	
+	public boolean remove(T element) {
+		
+		boolean found = false;
+		int i = 0;
+		for (; i < data.length; i++) {
+			if ( data[i].equals(element) ) {
+				found = true;
+				break;
+			}
+		}
+		if (! found) { return false; }
+		
+		Object[] temp = new Object[data.length - 1];
+		int j = 0;
+		for (; j < i; j++) {
+			temp[j] = data[j];
+		}
+		j++;
+		for (; j < data.length; j++) {
+			temp[j-1] = data[j];
+		}
+		return true;
+	}
+	
+	public boolean remove(int index) {
+		if (index < data.length && index >= 0) {
+			Object[] temp = new Object[data.length - 1];
+			int j = 0;
+			for (; j < index; j++) {
+				temp[j] = data[j];
+			}
+			j++;
+			for (; j < data.length; j++) {
+				temp[j-1] = data[j];
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	
+	@Override
+	public Iterator<T> iterator() {
+		Iterator<T> iter = new Iterator<T>() {
+			
+			private int pos = 0;
+			
+			@Override
+			public boolean hasNext() {
+				return pos < default_size && data[pos] != null;
+			}
+			
+			@Override
+			public T next() throws NoSuchElementException {
+				pos++;
+				return (T) data[pos];
+			}
+			
+			@Override
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+		};
+		return iter;
+	}
+
+}
