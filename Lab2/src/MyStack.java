5c0e3e686d021c672468c3b1fd3f0286ffc834a5
Daniel Wojciechowski <wojciechowski.daniel.1998@gmail.com>
2018-03-08 01:25:03 +0100

    adding Lab2

---
+++ Lab2/src/MyStack.java
@@ -0,0 +1,48 @@
+import java.util.EmptyStackException;
+
+public class MyStack {
+
+	private Object[] stack;
+	private int maxSize;
+	private int top;
+	//LIFO last-in-first-out
+	
+	public MyStack(int size) {
+		maxSize = size;
+		stack = new Object[maxSize];
+		top = -1;
+	}
+	
+	public Object push(Object item) {
+		
+	}
+	
+	public Object pop() {
+		
+	}
+	
+	public Object peek() throws EmptyStackException {
+		
+	}
+	
+	public boolean isEmpty() {
+		
+	}
+	
+	public int size() {
+		
+	}
+	
+	public boolean clear() {
+		
+	}
+	
+	public boolean contains(Object item) {
+		
+	}
+	
+	public int indexOf(Object item) {
+		
+	}
+	
+}
